306b8816338e375563ffb1af1d93474cb3df6a78
drkenni <tanyromachka1@mail.ru>
2023-04-26 00:03:50 +0300

    new func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def up_letter(text):
+    """Возводит все буквы в большие"""
     return text.upper()
+
+
+def up_first_letter(text):
+    """Функция возводит первую букву заглавной"""
+    return text.title()
